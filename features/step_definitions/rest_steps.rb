ab6fcf871cd00267fd480bcc7f43839970563285
Jay McCure <jmccure@gmail.com>
2014-04-25 20:40:35 +1000

    commit6

--- features/step_definitions/rest_steps.rb
+++ features/step_definitions/rest_steps.rb
@@ -1,5 +1,5 @@
-When(/^the client requests GET \/fruits$/) do
-  pending # express the regexp above with the code you wish you had
+When(/^the client requests GET (.*)$/) do |path|
+  get(path)
 end
 
 Then(/^the response should be JSON:$/) do |string|
