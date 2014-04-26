c447ca75480ae792adf3734fcd0e42aa784f73fe
Jay McCure <jmccure@gmail.com>
2014-04-26 23:36:07 +1000

    pg213

--- features/step_definitions/rest_steps.rb
+++ features/step_definitions/rest_steps.rb
@@ -1,7 +1,9 @@
+require 'rack'
+
 When(/^the client requests GET (.*)$/) do |path|
   get(path)
 end
 
-Then(/^the response should be JSON:$/) do |string|
-  pending # express the regexp above with the code you wish you had
+Then(/^the response should be JSON:$/) do |json|
+  JSON.parse(last_response.body).should eq(JSON.parse(json))
 end
